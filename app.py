df2cb96909067384709eff3ab8d955d9d80a5342
Mohnish124 <mohnishkundnani12@gmail.com>
2026-07-28 10:12:44 +0530

    Added user authentication feature message

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World"
-
+    return "Hello World - User Authentication Feature"
 if __name__ == "__main__":
     app.run(debug=True)
